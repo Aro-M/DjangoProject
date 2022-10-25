edbe011334ada4c8eb9c407812890f97cb9c77d1
Aro-M <ar.militosyan.97@mail.ru>
2022-10-25 12:16:14 +0400

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.homePageView,name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
+    path("", views.HomePageView.as_view(), name="home"),
+
 ]
